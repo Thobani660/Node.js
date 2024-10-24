22929f6ca5b3b7431c79a5bc38597ad8300dfa32
Thobani660 <zondit660@gmail.com>
2024-10-24 10:22:10 +0200

    Using postman I have a functional Node.js server

--- my-node-server/server.js
+++ my-node-server/server.js
@@ -16,19 +16,19 @@ const server = http.createServer((req, res) => {
     // Routes
     if (url === '/' && method === 'GET') {
       res.statusCode = 200;
-      res.end(JSON.stringify({ message: 'Welcome to the Node.js Server!' }));
+      res.end(JSON.stringify({ message: 'Welcome to the Node.js Server!' })); //Returns a welcome message.
     } else if ((url === '/' && method === 'PUT') || (url === '/' && method === 'PATCH')) {
       const data = body ? JSON.parse(body) : {};
       res.statusCode = 200;
-      res.end(JSON.stringify({ message: 'Successfully updated', option: data.option }));
+      res.end(JSON.stringify({ message: 'Successfully updated', option: data.option })); //Updates data based on the request body.
     } else if (url === '/' && method === 'POST') {
       const data = body ? JSON.parse(body) : {};
       res.statusCode = 201;
-      res.end(JSON.stringify({ message: 'Successfully created', option: data.option }));
+      res.end(JSON.stringify({ message: 'Successfully created', option: data.option })); //Handles the creation of data.
     } else if (url === '/' && method === 'DELETE') {
       const data = body ? JSON.parse(body) : {};
       res.statusCode = 200;
-      res.end(JSON.stringify({ message: 'Successfully deleted', option: data.option }));
+      res.end(JSON.stringify({ message: 'Successfully deleted', option: data.option })); //Deletes data and responds with a success message
     } else {
       res.statusCode = 404;
       res.end(JSON.stringify({ error: 'Route not found' }));
