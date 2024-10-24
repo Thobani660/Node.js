b94a38b4c36f7399754de91b782cb0d876497590
Thobani660 <zondit660@gmail.com>
2024-10-24 10:15:35 +0200

    creating the server,The request object contains information about the incoming HTTP request

--- my-node-server/server.js
+++ my-node-server/server.js
@@ -5,6 +5,7 @@ const server = http.createServer((req, res) => {
   const { method, url } = req;
   let body = '';
 
+  // Collect data chunks from the request body
   req.on('data', chunk => {
     body += chunk.toString();
   });
@@ -12,25 +13,32 @@ const server = http.createServer((req, res) => {
   req.on('end', () => {
     res.setHeader('Content-Type', 'application/json');
 
+    // Routes
     if (url === '/' && method === 'GET') {
       res.statusCode = 200;
       res.end(JSON.stringify({ message: 'Welcome to the Node.js Server!' }));
     } else if ((url === '/' && method === 'PUT') || (url === '/' && method === 'PATCH')) {
-      const data = JSON.parse(body);
+      const data = body ? JSON.parse(body) : {};
       res.statusCode = 200;
       res.end(JSON.stringify({ message: 'Successfully updated', option: data.option }));
     } else if (url === '/' && method === 'POST') {
-      const data = JSON.parse(body);
+      const data = body ? JSON.parse(body) : {};
       res.statusCode = 201;
       res.end(JSON.stringify({ message: 'Successfully created', option: data.option }));
     } else if (url === '/' && method === 'DELETE') {
-      const data = JSON.parse(body);
+      const data = body ? JSON.parse(body) : {};
       res.statusCode = 200;
       res.end(JSON.stringify({ message: 'Successfully deleted', option: data.option }));
     } else {
       res.statusCode = 404;
       res.end(JSON.stringify({ error: 'Route not found' }));
     }
+  });
+
+  // Error handling
+  req.on('error', (err) => {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ error: 'Server error', details: err.message }));
   });
 });
 
